59055b03e63a65afbad08fe10d39bed1c4ba3e3e
z3kan <mr.r0b0t0x17@gmail.com>
2023-03-10 23:21:38 +0300

    simple changes هin colors

--- main_movie.py
+++ main_movie.py
@@ -11,16 +11,13 @@ class Main:
 	# cimaleak_url = 'https://cimalek.to/?s'
 
 	def __init__(self):
-
 		self.X = input(colored('Set Name Of Movie: ', 'green')).replace(' ','+')
 		self.request = requests.get(f'https://cimalek.to/?s={self.X}')
 		self.soup = BeautifulSoup(self.request.content, 'lxml')
 		self.data = self.soup.find_all('div', {'class':'item'})
 
 	def analysis_data(self):
-
 		for i in range(len(self.data)):
-
 			self.title_movie = self.data[i].find('div', {'class':'title'}).text.strip()
 			for link in self.data[i].find_all('a'):
 				self.href = link.attrs['href']
@@ -30,21 +27,13 @@ class Main:
 
 
 	@classmethod
-	def loop(cls):
+	def user_command(cls):
 		if Main.list_name != 0:
-			while True:
 				user_input = input(colored('Set Your Movie NUM: ', 'green'))
-				print (Main.list_link[int(user_input)])
-
-
+				print (colored('The Link: ','light_blue')+colored(Main.list_link[int(user_input)],'light_cyan'))
 
 if __name__ == '__main__':
 
 	op1 = Main()
 	op1.analysis_data()
-	op1.loop()
-	
-
-
-
-
+	op1.user_command()
